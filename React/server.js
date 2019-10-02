1e10aaeb5599a6da07c5d15cc361e8bec14be62d
Espumita <davidfalconnaranjo@gmail.com>
2019-10-02 10:46:24 +0100

    Delete       //historyApiFallback(),

--- React/server.js
+++ React/server.js
@@ -2,7 +2,6 @@ import browserSync from 'browser-sync';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 //import webpackHotMiddleware from 'webpack-hot-middleware';
-//import historyApiFallback from 'connect-history-api-fallback';
 import config from './webpack.config';
 
 const bundler = webpack({mode: 'development', ...config});
@@ -13,7 +12,6 @@ browserSync({
   server: {
     baseDir: 'src',
     middleware: [
-      //historyApiFallback(),
       webpackDevMiddleware(bundler, {
         publicPath: config.output.publicPath,
         noInfo: false,
