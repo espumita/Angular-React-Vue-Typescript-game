69c830e8040ba07c723954d0e1c3ceec083d952f
Espumita <davidfalconnaranjo@gmail.com>
2019-10-04 16:10:55 +0100

    Add number to cell type

--- React/src/components/Cell.tsx
+++ React/src/components/Cell.tsx
@@ -7,11 +7,24 @@ function getColor(type : CellType) {
   return 'yellow'
 }
 
+function getNumber(type : CellType) {
+  if (type === CellType.OneMineClose) return '1'
+  if (type === CellType.TwoMinesClose) return '2'
+  if (type === CellType.TreeMinesClose) return '3'
+  if (type === CellType.FourMinesClose) return '4'
+  if (type === CellType.FiveMinesClose) return '5'
+  if (type === CellType.SixMinesClose) return '6'
+  if (type === CellType.SevenMinesClose) return '7'
+  if (type === CellType.EightMinesClose) return '8'
+  return ''
+}
+
 const Cell = function Cell(props: Positionable & { clickAction : Function, type: CellType }) {
     const color = getColor(props.type)
+    const number = getNumber(props.type)
     return (
       <div onClick={() => props.clickAction()} style={{ width: '40px', height: '40px', backgroundColor: color, borderBlockColor: 'black', borderStyle: 'solid', borderWidth: '1px'}}>
-        ({props.x},{props.y})
+        {number}
       </div>
     )
   }
--- React/src/store/initialState.ts
+++ React/src/store/initialState.ts
@@ -31,7 +31,15 @@ export enum GameState {
 export enum CellType {
     None = 0,
     EmptyCell = 1,
-    Mine = 2
+    Mine = 2,
+    OneMineClose = 3,
+    TwoMinesClose = 4,
+    TreeMinesClose = 5,
+    FourMinesClose = 6,
+    FiveMinesClose = 7,
+    SixMinesClose = 8,
+    SevenMinesClose = 9,
+    EightMinesClose = 10
 }
 
 
