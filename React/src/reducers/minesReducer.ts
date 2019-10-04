27eb59f2dbcdb1fdecc93d572c2e484883935885
Espumita <davidfalconnaranjo@gmail.com>
2019-10-04 12:12:47 +0100

    Deploy mines

--- React/src/reducers/minesReducer.ts
+++ React/src/reducers/minesReducer.ts
@@ -8,7 +8,15 @@ export default (state: Mine[] = undefined, action: DeployMinesAction) => {
     switch(action.type){
         case DEPLOY_MINES_ACTION: {
             console.log('DEPLOYING MINES', action.difficulty.minesNumber)
-            
+            const newMines : Mine[] = []
+            while (newMines.length < action.difficulty.minesNumber){
+                const a = Math.floor(Math.random() * (action.difficulty.boardWidth))
+                const b = Math.floor(Math.random() * (action.difficulty.boardHeight))
+                if (!state.some(x => x.position === { x: a, y: b})){
+                    newMines.push({ position: {x: a, y: b}})
+                }
+            }
+            return newMines
         }   
         default: return state
     }
