06dcaafe8ff5bc7433e5b3a9d000fa8cb8c66876
Espumita <davidfalconnaranjo@gmail.com>
2019-10-12 00:00:34 +0100

    Green: set mines and perimeters when react to deploy mines action for one mine

--- React/tests/reducers/minesReducer.spec.ts
+++ React/tests/reducers/minesReducer.spec.ts
@@ -1,5 +1,5 @@
 import reducer from '../../src/reducers/minesReducer' 
-import { BeginerDifficulty } from '../../src/store/initialState'
+import { BeginerDifficulty, Difficulty } from '../../src/store/initialState'
 import { DeployMinesAction } from '../../src/actions/gameActions'
 import { DEPLOY_MINES_ACTION } from '../../src/actions/actionsTypes'
 
@@ -15,6 +15,27 @@ describe('Mines reducer should', () => {
 
         expect(newState.positions.length).toBe(0)
         expect(newState.perimeterCells.length).toBe(0)
+    })
+
+    test('set mines and perimeters when react to deploy mines action for one mine', () => {
+        const deployMinesAction : DeployMinesAction = {
+            type: DEPLOY_MINES_ACTION
+        }
+        const aDifficutly : Difficulty = {
+            boardWidth: 2,
+            boardHeight: 1,
+            minesNumber: 1
+        }
+        const state = {
+            positions: [],
+            perimeterCells: []
+        }
+
+        const newState = reducer(state, deployMinesAction, aDifficutly)
+
+        expect(newState.positions.length).toBe(aDifficutly.minesNumber)
+        expect(newState.perimeterCells.length).toBe(1)
+        expect(newState.perimeterCells[0].numberOfClosestsMines).toBe(1)
     })
 
 })
