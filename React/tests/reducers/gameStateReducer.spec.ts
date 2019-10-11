d0691dee73640fa1b6917abde02b9377877726b4
Espumita <davidfalconnaranjo@gmail.com>
2019-10-11 23:52:24 +0100

    ignore

--- React/tests/reducers/gameStateReducer.spec.ts
+++ React/tests/reducers/gameStateReducer.spec.ts
@@ -6,7 +6,7 @@ import { START_GAME_ACTION } from '../../src/actions/actionsTypes'
 describe('Game state reducer should set game state to', () => {
 
     test('not started at the beginning', () => {
-        const startGaneAction : StartGaneAction= {
+        const startGaneAction : StartGaneAction = {
             type: START_GAME_ACTION,
             position: new Position(0, 0)
         }
@@ -18,7 +18,7 @@ describe('Game state reducer should set game state to', () => {
 
     test('started when the game start', () => {
         const state = GameState.NotStarted
-        const startGaneAction : StartGaneAction= {
+        const startGaneAction : StartGaneAction = {
             type: START_GAME_ACTION,
             position: new Position(0, 0)
         }
