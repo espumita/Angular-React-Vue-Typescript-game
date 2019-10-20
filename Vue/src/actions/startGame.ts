721542a1947be94fc17accf0c9d42db84366ea77
Espumita <davidfalconnaranjo@gmail.com>
2019-10-20 23:56:14 +0100

    Make first movement when start the game

--- Vue/src/actions/startGame.ts
+++ Vue/src/actions/startGame.ts
@@ -1,13 +1,15 @@
 import { START_GAME } from './actionsTypes'
+import { MAKE_MOVEMENT } from './actionsTypes'
 import { START_GAME_MUTATION } from '../mutations/mutationTypes'
 import { Position } from '../model'
 
 export function distpatchCreateStartGameAction(position: Position){
     return (dispatch: Function) => {
         dispatch(START_GAME, position)
+        dispatch(MAKE_MOVEMENT, position)
     }
 }
 
 export default {
-    [START_GAME]: (context: any, position : Position) =>context.commit(START_GAME_MUTATION, position)
+    [START_GAME]: (context: any, position : Position) => context.commit(START_GAME_MUTATION, position)
 }
