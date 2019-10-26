bf3930f442d61e1874de629fb960820a4f66596c
Espumita <davidfalconnaranjo@gmail.com>
2019-10-26 10:02:40 +0100

    Add model

---
+++ Angular/src/model/cellType.ts
@@ -0,0 +1,13 @@
+export enum CellType {
+    None = 0,
+    EmptyCell = 1,
+    Mine = 2,
+    OneMineClose = 3,
+    TwoMinesClose = 4,
+    TreeMinesClose = 5,
+    FourMinesClose = 6,
+    FiveMinesClose = 7,
+    SixMinesClose = 8,
+    SevenMinesClose = 9,
+    EightMinesClose = 10
+}
