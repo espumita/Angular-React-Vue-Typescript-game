4e456965b9211233c92a656ca18e45d6215750ba
Espumita <davidfalconnaranjo@gmail.com>
2019-10-29 18:53:27 +0000

    Add Devtools to Module

--- Angular/src/components/root/root.module.ts
+++ Angular/src/components/root/root.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { RootComponent } from './root.component';
-import { BoardComponent } from '../board/board.component';
-import { CellComponent } from '../cell/cell.component';
-import { StoreModule } from '@ngrx/store';
+import { RootComponent } from './root.component'
+import { BoardComponent } from '../board/board.component'
+import { CellComponent } from '../cell/cell.component'
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 
 import { difficultyReducer } from '../../reducers/difficulty.reducer'
 
@@ -19,7 +20,8 @@ import { difficultyReducer } from '../../reducers/difficulty.reducer'
     BrowserModule,
     StoreModule.forRoot({
       difficulty: difficultyReducer
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 10})
   ],
   providers: [],
   bootstrap: [RootComponent]
