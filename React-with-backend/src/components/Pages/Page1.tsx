276f79012f75d203b9615a1710a4bf94c57aec70
Espumita <davidfalconnaranjo@gmail.com>
2019-12-03 19:18:55 +0000

    Add text to pages

--- React-with-backend/src/components/Pages/Page1.tsx
+++ React-with-backend/src/components/Pages/Page1.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 const Page1 = () => {
     return(
         <div>
+            <div>This is Page1</div>
             <div>PAGE1</div>
             <div>PAGE1</div>
             <div>PAGE1</div>
--- React-with-backend/src/components/Pages/Page2.tsx
+++ React-with-backend/src/components/Pages/Page2.tsx
@@ -3,6 +3,7 @@ import * as React from 'react'
 const Page2 = () => {
     return(
         <div>
+            <div>This is Page2</div>
             <div>PAGE2</div>
             <div>PAGE2</div>
             <div>PAGE2</div>
